58310e54af9490f3eae87f616255d1734f81bf08
Ryan Boyd <114888159+ryantylerboyd@users.noreply.github.com>
2023-03-20 13:51:06 +1000

    migraded database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_093336) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_032208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.integer "item_id"
+    t.string "name"
+    t.string "examine"
+    t.string "destroy_message"
+    t.boolean "tradeable"
+    t.integer "weight"
+    t.integer "attack_speed"
+    t.integer "attack_stab"
+    t.integer "attack_slash"
+    t.integer "attack_crush"
+    t.integer "attack_magic"
+    t.integer "attack_range"
+    t.integer "defence_stab"
+    t.integer "defence_slash"
+    t.integer "defence_crush"
+    t.integer "defence_magic"
+    t.integer "defence_range"
+    t.integer "prayer"
+    t.integer "range_strength"
+    t.integer "magic_damage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
